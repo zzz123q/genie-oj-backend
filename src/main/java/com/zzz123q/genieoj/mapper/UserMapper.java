bff13c913a03a871909d568c0e2babdbf3201e2d
zzz123q <1352929539@qq.com>
2024-08-26 16:30:00 +0800

    项目初始化

---
+++ src/main/java/com/zzz123q/genieoj/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.zzz123q.genieoj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zzz123q.genieoj.model.entity.User;
+
+/**
+ * 用户(user)数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/main/java/com/zzz123q/genieoj/result/BaseResponse.java
@@ -0,0 +1,33 @@
+package com.zzz123q.genieoj.result;
+
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ * 通用返回类
+ *
+ * @param <T>
+ */
+@Data
+public class BaseResponse<T> implements Serializable {
+
+    private int code;
+
+    private T data;
+
+    private String message;
+
+    public BaseResponse(int code, T data, String messagse) {
+        this.code = code;
+        this.data = data;
+        this.message = message;
+    }
+
+    public BaseResponse(int code, T data) {
+        this(code, data, "");
+    }
+
+    public BaseResponse(ErrorCode errorCode) {
+        this(errorCode.getCode(), null, errorCode.getMessage());
+    }
+}
