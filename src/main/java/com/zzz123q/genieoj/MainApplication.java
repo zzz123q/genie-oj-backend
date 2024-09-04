2879e9fdcfd21396a2ecaa7c43d74d753870af3b
zzz123q <1352929539@qq.com>
2024-09-04 14:50:14 +0800

    加入Redis

--- src/main/java/com/zzz123q/genieoj/MainApplication.java
+++ src/main/java/com/zzz123q/genieoj/MainApplication.java
@@ -3,15 +3,13 @@ package com.zzz123q.genieoj;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 主类(项目启动入口)
  */
-// TODO 如需开启 Redis，须移除 exclude 中的内容
-@SpringBootApplication(exclude = { RedisAutoConfiguration.class })
+@SpringBootApplication
 @MapperScan("com.zzz123q.genieoj.mapper")
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
